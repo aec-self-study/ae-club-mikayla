49bc97995b665a794df48d32e3b2234d19fd8307
msegu134 <mdsegura02@gmail.com>
2024-08-28 16:46:44 +0000

    add_customers_query

--- coffee_shop_mikayla_segura/models/customers.sql
+++ coffee_shop_mikayla_segura/models/customers.sql
@@ -7,19 +7,19 @@ WITH customer_orders AS (
 
     SELECT
         customer_id,
-        count(*) AS n_orders,
-        min(created_at) AS first_order_at
-    FROM `analytics-engineers-club.coffee_shop.orders` 
+        count(*)                                AS n_orders,
+        min(created_at)                         AS first_order_at
+    FROM  `analytics-engineers-club.coffee_shop.orders`
     GROUP BY 1
 
 ),
 
 final AS (
     SELECT 
-        customers.id AS customer_id,
+        customers.id                            AS customer_id,
         customers.name,
         customers.email,
-        coalesce(customer_orders.n_orders, 0) AS n_orders,
+        coalesce(customer_orders.n_orders, 0)   AS n_orders,
         customer_orders.first_order_at
     FROM `analytics-engineers-club.coffee_shop.customers` AS customers
     LEFT JOIN customer_orders
