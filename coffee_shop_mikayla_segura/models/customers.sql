773ba097b896dee07a34c4facfa3ff8afcd36d50
msegu134 <mdsegura02@gmail.com>
2024-08-28 17:01:37 +0000

    add_customers_query

--- coffee_shop_mikayla_segura/models/customers.sql
+++ coffee_shop_mikayla_segura/models/customers.sql
@@ -9,7 +9,7 @@ WITH customer_orders AS (
         customer_id,
         count(*)                                AS n_orders,
         min(created_at)                         AS first_order_at
-    FROM  `analytics-engineers-club.coffee_shop.orders`
+    FROM  {{ source('coffee_shop', 'orders') }} 
     GROUP BY 1
 
 ),
@@ -21,7 +21,7 @@ final AS (
         customers.email,
         coalesce(customer_orders.n_orders, 0)   AS n_orders,
         customer_orders.first_order_at
-    FROM `analytics-engineers-club.coffee_shop.customers` AS customers
+    FROM{{ source('coffee_shop', 'customers') }} AS customers
     LEFT JOIN customer_orders
         ON customers.id = customer_orders.customer_id 
     LIMIT 5
